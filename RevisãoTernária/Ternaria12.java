f3d934341b603eb3b221f3e8ff94480c8a6a9129
Davi Soares <Davi.soares2008@icloud.com>
2025-02-18 19:45:58 -0300

    Delete RevisãoTernária directory

--- RevisãoTernária/Ternaria12.java
+++
@@ -1,9 +0,0 @@
-package Ternaria;
-
-public class Ternaria12 {
-    public static void main(String[] args) {
-
-        int a = 10, b = 20;
-        System.out.println(" Maior número: " + (a > b ? a : b));
-    }
-}
