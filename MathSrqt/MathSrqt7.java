4ca497cb1c12b791eb0bb2b98b9aa327c006e4e5
Davi Soares <Davi.soares2008@icloud.com>
2025-02-18 19:45:35 -0300

    Delete MathSrqt directory

--- MathSrqt/MathSrqt7.java
+++
@@ -1,9 +0,0 @@
-package org.Laboratorio.br;
-
-public class MathSrqt7 {
-    public static void main(String[] args) {
-        double number = 0.25;
-        double SquareRoot = Math.sqrt(number);
-        System.out.println("The SquareRoot of " + number + " is: " + SquareRoot);
-    }
-}
